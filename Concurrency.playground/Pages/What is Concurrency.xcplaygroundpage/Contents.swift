e687da0a60650d10e15ba663797b8065930fb4e1
Giovanni Noa <self@gionoa.dev>
2019-12-08 15:33:33 -0500

    Add Playground

---
+++ Concurrency.playground/Pages/What is Concurrency.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+/*:
+ # What is Concurrency?
+  - The ability of different parts or units of a program, algorithm, or problem to be executed out-of-order or in partial order, without affecting the final outcome.
+ */
