d4c177be595f9fbc2d04955b038724770b6283e3
Iván <me@ivancr.com>
2019-12-08 22:46:25 -0800

    Added Async vs Sync page

---
+++ Concurrency.playground/Pages/Sync vs Async.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let queue = DispatchQueue(label: "com.swiftmiami.parceritos",
+                          attributes: .concurrent)
+
+let op1: () -> Void = {
+    Thread.sleep(forTimeInterval: 3)
+    print("Operation 1 Complete!")
+}
+
+let op2: () -> Void = {
+    Thread.sleep(forTimeInterval: 1)
+    print("Operation 2 Complete!")
+}
+
+func runSync() {
+    print("Started Operation 1")
+    queue.sync {
+        op1()
+    }
+
+    print("Started Operation 2")
+    queue.sync {
+        op2()
+    }
+}
+
+func runAsync() {
+    print("Started Operation 1")
+    queue.async {
+        op1()
+    }
+
+    print("Started Operation 2")
+    queue.async {
+        op2()
+    }
+}
+
+runSync()
+//runAsync()
+
+//: [Next](@next)
--- Concurrency.playground/Pages/What is Concurrency.xcplaygroundpage/Contents.swift
+++ Concurrency.playground/Pages/What is Concurrency.xcplaygroundpage/Contents.swift
@@ -2,3 +2,5 @@
  # What is Concurrency?
   - The ability of different parts or units of a program, algorithm, or problem to be executed out-of-order or in partial order, without affecting the final outcome.
  */
+
+//: [Next](@next)
