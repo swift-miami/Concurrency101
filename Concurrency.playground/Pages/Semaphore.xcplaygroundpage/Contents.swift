4f29ebaca117a1d69ddc73ef64317dcb6005b01b
Giovanni Noa <self@gionoa.dev>
2019-12-12 22:41:35 -0500

    Added Semaphore page

---
+++ Concurrency.playground/Pages/Semaphore.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let queue = DispatchQueue(label: "com.swiftmiami.semawho", attributes: .concurrent)
+let semaphore = DispatchSemaphore(value: 0)
+
+let op1: () -> Void = {
+    Thread.sleep(forTimeInterval: .pi)
+    print("Operation 1 Complete!")
+}
+
+let op2: () -> Void = {
+    Thread.sleep(forTimeInterval: 1)
+    print("Operation 2 Complete!")
+}
+
+func runSemaphore() {
+    print("Started Operation 1")
+    queue.async {
+        op1()
+        semaphore.signal()
+    }
+    
+    semaphore.wait()
+    
+    print("Started Operation 2")
+    queue.async {
+        op2()
+    }
+}
+
+runSemaphore()
+
+//: [Next](@next)
